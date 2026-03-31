4a0193c61f4e359f3078779e8182117eb85ab7dc
519zitouni <zitouni.contact@gmail.com>
2026-03-31 17:48:57 +0200

    Main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import os
+import requests
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, data=data)
+
 def main():
     send_telegram("TEST OK 🚀")
+
+if __name__ == "__main__":
+    main()
